caac32aea9c8b382dfbd46f067d4037acfee0e47
jpark9595 <56580445+jpark9595@users.noreply.github.com>
2020-02-12 20:56:07 +0900

    debouncing 기능 추가

--- capstone_no.1.ino
+++ capstone_no.1.ino
@@ -1,26 +1,55 @@
-int state=0; //0: 시작 전 대기상태, 1: 내려가고있는 중, 2: 정지 후 대기, 3: 다 되서 올라가는 중
-int switch_pin[3]={7,8,9};
+int state = 0; //0: 시작 전 대기상태, 1: 내려가고있는 중, 2: 정지 후 대기, 3: 다 되서 올라가는 중
+int button_pin[4] = {10, 11, 12, 13};
+///////these lines are for debouncing buttons///////////
+boolean last_button_state[4] = {0, 0, 0, 0};
+boolean button_state[4] = {0, 0, 0, 0};
+unsigned long last_debounce[4] = {0, 0, 0, 0};
+const int debounce_delay = 50;
+/////////////////////////////////////////////////////////
 
 
 void setup() {
-  // put your setup code here, to run once:
-  for(i=1;i<3;i++){//스위치 핀 3개 인풋 활성화
-    pinMode(switch_pin[i], INPUT);
+  Serial.begin(115200);
+  for (int i = 1; i < 4; i++) { //버튼 핀 4개 인풋 활성화
+    pinMode(button_pin[i], INPUT);
   }
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  switch (state) {
-  case label1:
-    // statements
-    break;
-  case label2:
-    // statements
-    break;
-  default:
-    // statements
-    break;
+  for (int i = 0; i < 4; i++) {
+    debouncing_button(i);
+
+    delay(10);
+  }
+
+  /* switch (state) {
+     case label1:
+       // statements
+       break;
+     case label2:
+       // statements
+       break;
+     default:
+       // statements
+       break;
+    }*/
+
 }
 
+///////////// 버튼 인식 부분 (debouncing) ////////////////
+void debouncing_button(int i) {
+  int reading = digitalRead(button_pin[i]);
+  if (reading != last_button_state[i])  //스위치의 이전과 지금 상태가 다르면
+    last_debounce[i] = millis();   //초를 기록합니다.
+
+  if ((millis() - last_debounce[i]) > debounce_delay) { //스위치 상태가 debounce_delay 이상 같으면
+    if (reading != button_state[i]) { //state와 상태가 다를경우 기록
+      button_state[i] = reading;
+      if (button_state[i] == LOW) { //LOW일때 작업 할당 -> pull-up 저항있음
+        Serial.print(i + 1);
+        Serial.println(" button pressed");
+      }
+    }
+  }
+  last_button_state[i] = reading;
 }
